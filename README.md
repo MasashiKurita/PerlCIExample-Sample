e736f69fdbf9a5c78f06969e4ff0577b0e4b2eca
Masashi Kurita <marty.marron@gmail.com>
2016-01-02 07:32:11 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+PerlCIExample::Sample - It's new $module
+
+# SYNOPSIS
+
+    use PerlCIExample::Sample;
+
+# DESCRIPTION
+
+PerlCIExample::Sample is ...
+
+# LICENSE
+
+Copyright (C) Masashi Kurita.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Masashi Kurita &lt;marty.marron@gmail.com>
---
+++ lib/PerlCIExample/Sample.pm
@@ -0,0 +1,39 @@
+package PerlCIExample::Sample;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+PerlCIExample::Sample - It's new $module
+
+=head1 SYNOPSIS
+
+    use PerlCIExample::Sample;
+
+=head1 DESCRIPTION
+
+PerlCIExample::Sample is ...
+
+=head1 LICENSE
+
+Copyright (C) Masashi Kurita.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Masashi Kurita E<lt>marty.marron@gmail.comE<gt>
+
+=cut
+
